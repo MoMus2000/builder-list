f30740919ba6a65d777cdfddc6eb1d0fc558f361
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-05-20 00:05:24 -0400

    Fix for ugly JSON list

--- builder_list/projects/models.py
+++ builder_list/projects/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+# Schema is Ugly, create a new model called Todos
+# On those todo's there can be an optional model called Comment
+
 class Project(models.Model):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
 class Design(models.Model):
-    todo_list = models.JSONField(default=list) # make back into a dict, if a comment key
-    # is set that means a comment has been written
+    todo_list = models.JSONField(default=list)
 
 class Roof(models.Model):
     todo_list = models.JSONField(default=list)
@@ -19,6 +21,13 @@ class F2(models.Model):
 
 class Basement(models.Model):
     todo_list = models.JSONField(default=list)
+
+class Comment(models.Model):
+    comment = models.Model(max_length=1000)
+
+class Todo(models.Model):
+    todo = models.CharField(max_length=200)
+    comment = models.ForeignKey(Comment, null=True, blank=True)
 
 class Checklist(models.Model):
     name     = models.CharField(max_length=200)
